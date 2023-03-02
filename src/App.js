0c578bc20c73413eeeb59895fb266893c0e74aed
SinghHridyansh <Hridyansh.360@gmail.com>
2023-03-02 21:23:09 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "../src/App.css";
-import Navbar from "./Component/Navbar";
 import Main from "./Component/Main";
 
 export default function App() {
   return (
     <div>
-      <Navbar />
+      <h3
+        style={{
+          backgroundColor: "#4472C4",
+          color: "white",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Hridyansh Weather App
+      </h3>
       <Main />
     </div>
   );
